1ad0ed69369b338a47bab5c1a071d0d6e039a12d
Edzell <derz@elidragon.com>
2022-04-24 11:27:52 +0200

    client

---
+++ client.go
@@ -0,0 +1,64 @@
+package telnetClient
+
+import (
+	"fmt"
+
+	"github.com/reiver/go-telnet"
+)
+
+type controller struct {
+	conn *telnet.Conn
+}
+
+func NewController(dest string) (*controller, error) {
+	conn, err := telnet.DialTo(dest)
+	if err != nil {
+		return nil, err
+	}
+
+	return &controller{conn: conn}, nil
+}
+
+func (c *controller) ReadUntil(b byte) ([]byte, error) {
+	var bytes []byte
+	notFound := true
+
+	for notFound {
+		nbyte := make([]byte, 1)
+		_, err := c.conn.Read(nbyte)
+		if err != nil {
+			return nil, err
+		}
+
+		if nbyte[0] == b {
+			notFound = false
+		} else {
+			bytes = append(bytes, nbyte...)
+		}
+	}
+
+	return bytes, nil
+}
+
+func (c *controller) Write(cmd string) error {
+	_, err := c.conn.Write([]byte(cmd + "\n"))
+	return err
+}
+
+func (c *controller) Exec(cmd string) (string, error) {
+	err := c.Write(cmd)
+	if err != nil {
+		return "", err
+	}
+
+	bytes, err := c.ReadUntil('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+func (c *controller) Close() error {
+	return c.conn.Close()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ev2-1/telnetClient
+
+go 1.18
+
+require (
+	github.com/reiver/go-oi v1.0.0 // indirect
+	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e // indirect
+)
